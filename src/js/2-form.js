8b898f6a30a1de3fac73e5eb23a52380b254c9d4
Maryna-Kashuba <144819587+Maryna-Kashuba@users.noreply.github.com>
2024-02-11 17:57:23 +0200

    create pages

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,34 @@
-console.log('Form');
+document.addEventListener('DOMContentLoaded', () => {
+    const feedbackForm = document.querySelector('.feedback-form');
+  
+    feedbackForm.addEventListener('input', () => {
+      const formData = {
+        email: feedbackForm.elements.email.value.trim(),
+        message: feedbackForm.elements.message.value.trim(),
+      };
+  
+      localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+    });
+  
+    const storedData = localStorage.getItem('feedback-form-state');
+  
+    if (storedData) {
+      const parsedData = JSON.parse(storedData);
+  
+      feedbackForm.elements.email.value = parsedData.email;
+      feedbackForm.elements.message.value = parsedData.message;
+    }
+  
+    feedbackForm.addEventListener('submit', (event) => {
+      event.preventDefault();
+  
+      const formData = {
+        email: feedbackForm.elements.email.value.trim(),
+        message: feedbackForm.elements.message.value.trim(),
+      };
+       console.log(formData);
+ 
+      localStorage.removeItem('feedback-form-state');
+      feedbackForm.reset();
+    });
+  });
